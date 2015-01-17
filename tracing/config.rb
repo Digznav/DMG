bb87affc4d2c2aaf80b8ac289e814483849dd8fb
Diego Miguel <ryu.orochi@gmail.com>
2015-01-17 15:49:07 -0600

    Add tracing project

---
+++ tracing/config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/scss"
+images_dir = "images"
+fonts_dir = "webfonts"
+javascripts_dir = "js"
+
+output_style = :nested
+relative_assets = true
+line_comments = false
+preferred_syntax = :scss
+asset_cache_buster :none
