eff34c95020876a03357b853da10b1afc8f6f9fc
Diego Miguel <ryu.orochi@gmail.com>
2015-03-07 13:36:36 -0600

    Delete condekit.config

--- test/js/jwebbck.js
+++ test/js/jwebbck.js
@@ -1,67 +1,95 @@
-"use strict";
-var elements = document.querySelectorAll("#components > div");
+'use strict';
 
-// function matchExp(string){
-// 	console.log(string);
-
-// 	var myRegExps = ["/\[Ff]orm\W/", "/\[Ff]ragment\W/", "/\[Cc]ontent\W/"];
-
-// 	for (var i = 0; i < Things.length; i++) {
-// 		Things[i]
-// 	};
-
-// 	myRegExps.forEach(function(reg, idx) {
-// 		if (string.match(reg)) { return idx; }
-// 		else { console.log("error"); }
-// 	});
-// };
-
-// fourcol
-
+var elements = document.querySelectorAll('#components > div');
 
 function matchExp(string){
-	var expresions = new Array(
-		/Form/g,
-		/Fragment/g,
-		/Content/g,
-		/Search/g
-	);
+  var expresions = new Array(
+	/Form/g,
+	/Fragment/g,
+	/Content/g,
+	/Search/g
+  );
 
-	return expresions.findIndex(function(regexp){
-		return regexp.test(string);
-	});
+  return expresions.findIndex(function(regexp){
+	return regexp.test(string);
+  });
+}
+
+function ContactForm() {
+	this.type = 'Contact Form';
+	this.uniqueID = false;
+	this.parentBlock = false;
+	this.className = false;
+	this.placement = false;
+	this.title = false;
+	this.noShowTitle = false;
+	this.linkedTo = false;
+	this.noPrefill = false;
+	this.formHandler = false;
+	this.returnURL = false;
+	this.displayFormdesc = false;
+	this.formdesc = false;
+	this.emailSubject = false;
+	this.emailText = false;
 
 }
 
-function getCF(fragCF){
-		console.log(fragCF);
+function checkifEmpty(el) {
+	var noEmpty = false;
+	if (el.type == 'text' && el.value !== '') {
+		noEmpty = el.value;
+	}
+	else if (el.type == 'checkbox' && el.checked) {
+		noEmpty = el.checked;
+	}
 
-};
+	return noEmpty;
+}
+
+function getCF(fragCF){
+	var allInputs = fragCF.querySelectorAll('.subblockform input[id]'),
+		 allSelect = fragCF.querySelectorAll('.subblockform select'),
+		 CFfrag = new ContactForm();
+		 // savedata = [];
+
+	// Array.prototype.forEach.call(allInputs, function(fldInput){
+	// 	savedata.push(fldInput.value);
+		console.log(allSelect);
+	// });
+
+	CFfrag.uniqueID = checkifEmpty(allInputs[0]);
+	CFfrag.parentBlock = checkifEmpty(allInputs[1]);
+	CFfrag.className = checkifEmpty(allInputs[2]);
+	CFfrag.placement = allSelect[0].value;
+	CFfrag.title = checkifEmpty(allInputs[3]);
+	CFfrag.className = checkifEmpty(allInputs[3]);
+	CFfrag.noShowTitle = checkifEmpty(allInputs[4]);
+	CFfrag.linkedTo = checkifEmpty(allInputs[5]);
+	// console.log( CFfrag.placement );
+	console.log( CFfrag );
+}
 
 // function createBackup() {
 
 // };
 
-// console.log("el matchExp regresa ", matchExp("Custom Fragment Options (Developer Only.)"));
-
 Array.prototype.forEach.call(elements, function(fragment){
-	var blckhead = fragment.querySelector(".subblockhead .blockgrouptitle a").innerHTML;
+	var blckhead = fragment.querySelector('.subblockhead .blockgrouptitle a').innerHTML;
 
 	if (matchExp(blckhead) === 0) {
-
 		getCF(fragment);
 	}
 
 	if (matchExp(blckhead) === 1) {
-		console.log("Fragment");
+		// console.log('Fragment');
 	}
 
 	if (matchExp(blckhead) === 2) {
-		console.log("Content");
+		// console.log('Content');
 	}
 
 	if (matchExp(blckhead) === 3) {
-		console.log("Search");
+		// console.log('Search');
 	}
 });
 
