09b933830da59f6b0001a2926c1382921a0a115b
Diego Miguel <ryu.orochi@gmail.com>
2016-11-03 20:27:53 -0600

    :memo: Samurai page files.

---
+++ samurai/config.rb
@@ -0,0 +1,17 @@
+distribution = :no
+
+require "breakpoint"
+# require "singularitygs"
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/scss"
+images_dir = "images"
+fonts_dir = "css/fonts"
+javascripts_dir = "js"
+
+output_style = (distribution == :yes) ? :compressed : :expanded;
+relative_assets = true
+line_comments = false
+preferred_syntax = :scss
+asset_cache_buster :none
